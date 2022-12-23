1a5d234c82421a99c6b911b3c7f73cb5fe72b587
jasi-ritesh <jasi.ritesh@gmail.com>
2022-12-23 12:18:49 +0530

    Refactor Test Cases

--- src/test/SimpleEngine_test.go
+++ src/test/SimpleEngine_test.go
@@ -13,7 +13,7 @@ func TestArithemeticEngine(t *testing.T) {
 	AddExpression(t, &api.Expression{Name: "First", Expr: "2+3"})
 	AddExpression(t, &api.Expression{Name: "Second", Expr: "10%3"})
 
-	//State-2
+	//Stage-2
 	//Engine is asked to evaluate all Expressions
 	Evaluate(t)
 
@@ -28,7 +28,7 @@ func TestArithemeticEngine(t *testing.T) {
 	assert.Contains(t, resultMap, "First")
 	//Map should have the correct value for the key called First
 	assert.Equal(t, "5", resultMap["First"])
-	//Map Should have a key called First
+	//Map Should have a key called Second
 	assert.Contains(t, resultMap, "Second")
 	//Map should have the correct value for the key called Second
 	assert.Equal(t, "1", resultMap["Second"])
@@ -52,6 +52,7 @@ func TestComparisonEngine(t *testing.T) {
 	//Add Expressions to Engine
 	AddExpression(t, &api.Expression{Name: "First", Expr: "3>2"})
 	AddExpression(t, &api.Expression{Name: "Second", Expr: "4<2"})
+	AddExpression(t, &api.Expression{Name: "Third", Expr: "100<=(50+60)"})
 
 	//State-2
 	//Engine is asked to evaluate all Expressions
@@ -63,15 +64,19 @@ func TestComparisonEngine(t *testing.T) {
 
 	//Stage-4
 	//Validate the Result
-	assert.Equal(t, 2, len(resultMap))
+	assert.Equal(t, 3, len(resultMap))
 	//Map Should have a key called First
 	assert.Contains(t, resultMap, "First")
 	//Map should have the correct value for the key called First
 	assert.Equal(t, "true", resultMap["First"])
-	//Map Should have a key called First
+	//Map Should have a key called Second
 	assert.Contains(t, resultMap, "Second")
 	//Map should have the correct value for the key called Second
 	assert.Equal(t, "false", resultMap["Second"])
+	//Map Should have a key called Third
+	assert.Contains(t, resultMap, "Third")
+	//Map should have the correct value for the key called Second
+	assert.Equal(t, "true", resultMap["Third"])
 
 	//Stage-5
 	//Delete the Result
@@ -81,7 +86,7 @@ func TestComparisonEngine(t *testing.T) {
 	//Stage-6
 	//Fetch Result From Engine
 	resultMap2 := FetchResult(t)
-	assert.Equal(t, 1, len(resultMap2))
+	assert.Equal(t, 2, len(resultMap2))
 
 	defer ClearEngine(t)
 }
